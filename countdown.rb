a1816e5e6550af3648f08460bec26a87dc9c8ff0
Sean Sutton Jr <sutton_mjbc@icloud.com>
2018-09-01 01:22:31 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,22 @@
-#write your code here
-
-def countdown
+def countdown(number)
+  number = 10
+  
+  while 0 < number
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    
+    break if number == 0
+  end 
+  
+  return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(count)
+
+  while count > 0
+  count -= 1
+  
+  sleep 1
+  end
+  
+end 
